e5a44643718e5fe6f4843c1d70822899290b11a4
@thiankiann <thiankiann@gmail.com>
2023-12-28 11:52:09 +0000

    first

---
+++ src/main/java/com/example/homework_1_m7_openfeign/Homework1M7OpenFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_1_m7_openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1M7OpenFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework1M7OpenFeignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_1_m7_openfeign/Homework1M7OpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_1_m7_openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework1M7OpenFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
